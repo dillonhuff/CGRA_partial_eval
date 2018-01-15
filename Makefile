33fa61f8fd9543cfe3733a212633166f10b0d9ef
Dillon Huff <dillonhuff@gmail.com>
2018-01-15 14:26:15 -0800

    Need to add support for signed comparators

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
 COREIR_HOME = /Users/dillon/CppWorkspace/coreir
 
-partial_evaluator:
+partial_evaluator: partial_evaluator.cpp
 	g++ -std=c++11 partial_evaluator.cpp -L$(COREIR_HOME)/lib/ -lcoreir -lcoreir-rtlil -o partial_evaluator
+
+clean:
+	rm -f partial_evaluator
--- partial_evaluator.cpp
+++ partial_evaluator.cpp
@@ -1,10 +1,58 @@
 #include "coreir.h"
 
+#include "coreir/libs/rtlil.h"
+#include "coreir/passes/transform/deletedeadinstances.h"
+#include "coreir/passes/transform/unpackconnections.h"
+#include "coreir/passes/transform/packconnections.h"
+
 using namespace CoreIR;
 using namespace std;
 
 int main() {
   Context* c = newContext();
 
+  CoreIRLoadLibrary_rtlil(c);
+
+  Module* topMod = nullptr;
+
+  if (!loadFromFile(c, "top.json", &topMod)) {
+    cout << "Could not Load from json!!" << endl;
+    c->die();
+  }
+
+  assert(topMod->hasDef());
+
+  c->runPasses({"rungenerators","removeconstduplicates","flatten","removeconstduplicates","packconnctions"});
+
+  ModuleDef* def = topMod->getDef();
+
+  // Extract the configuration subcircuit
+  vector<Wireable*> subCircuitPorts{def->sel("self")->sel("config_addr"),
+      def->sel("self")->sel("config_data"),
+      def->sel("self")->sel("clk"),
+      def->sel("self")->sel("reset")};
+
+  auto subCircuitInstances =
+    extractSubcircuit(topMod, subCircuitPorts);
+
+  cout << "# of instances in subciruit = " << subCircuitInstances.size() << endl;
+
+  // Create the subcircuit for the config
+  addSubcircuitModule("topMod_config",
+                      topMod,
+                      subCircuitPorts,
+                      subCircuitInstances,
+                      c,
+                      c->getGlobal());
+
+  Module* topMod_conf =
+    c->getGlobal()->getModule("topMod_config");
+
+  // cout << "topMod_config interface" << endl;
+  // cout << topMod_conf->toString() << endl;
+
+  assert(topMod_conf != nullptr);
+  assert(topMod_conf->hasDef());
+  
   deleteContext(c);
 }
