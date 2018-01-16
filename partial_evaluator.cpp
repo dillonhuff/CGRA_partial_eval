ad013a68a7c887cb4f3312d2552c4ead591e3ec8
Dillon Huff <dillonhuff@gmail.com>
2018-01-15 16:03:54 -0800

    Topological sorting fails for the configuration state. Might be the result of logic outputs from pes, but Im not sure

--- partial_evaluator.cpp
+++ partial_evaluator.cpp
@@ -23,10 +23,12 @@ int main() {
   assert(topMod->hasDef());
 
   c->runPasses({"rungenerators",
-        "removeconstduplicates",
+        //"removeconstduplicates",
         "flatten",
-        "removeconstduplicates",
-        "packconnctions"});
+        //"removeconstduplicates",
+        "packconnections"});
+
+  cout << "Done with packing connections" << endl;
 
   ModuleDef* def = topMod->getDef();
 
@@ -52,11 +54,46 @@ int main() {
   Module* topMod_conf =
     c->getGlobal()->getModule("topMod_config");
 
+  
   // cout << "topMod_config interface" << endl;
   // cout << topMod_conf->toString() << endl;
 
   assert(topMod_conf != nullptr);
   assert(topMod_conf->hasDef());
   
+  cout << "Saving the config circuit" << endl;
+  if (!saveToFile(c->getGlobal(), "topModConfig.json", topMod_conf)) {
+    cout << "Could not save to json!!" << endl;
+    c->die();
+  }
+
+  cout << "Clockifying the configuration circuit interface" << endl;
+
+  c->runPasses({"clockifyinterface"});
+
+  cout << "Building simulator state for config" << endl;
+
+  SimulatorState state(topMod_conf);
+  state.setClock("self.clk", 0, 1);
+  state.setValue("self.reset", BitVec(1, 0));
+
+  cout << " Done building simulator state" << endl;
+
+  cout << "Loading configuration state" << endl;
+
+  std::ifstream configFile("./bitstream/shell_bitstream.bs");
+  std::string str((std::istreambuf_iterator<char>(configFile)),
+                  std::istreambuf_iterator<char>());
+
+  cout << "Config file text" << endl;
+  cout << str << endl;
+
+  // TODO: Split config into lines
+
+  // TODO: Load simulate config line by line
+
+  // TODO: Partially evaluate the original top module by config
+
+  // TODO: Simulate the partially evaluated circuit
   deleteContext(c);
 }
