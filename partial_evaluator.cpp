8eb22e946007cf1e34d46d6f0b8de82fb760aec2
Dillon Huff <dillonhuff@gmail.com>
2018-01-16 20:16:57 -0800

    Can now process the top module into a config

--- partial_evaluator.cpp
+++ partial_evaluator.cpp
@@ -42,101 +42,6 @@ std::vector<std::string> splitString(const std::string& str,
     return strings;
 }
 
-void processTop(const std::string& fileName,
-                const std::string& topModName) {
-  Context* c = newContext();
-
-  CoreIRLoadLibrary_rtlil(c);
-
-  Module* topMod = nullptr;
-
-  if (!loadFromFile(c, fileName, &topMod)) {
-    cout << "Could not Load from json!!" << endl;
-    c->die();
-  }
-
-  topMod = c->getGlobal()->getModule(topModName);
-  c->setTop(topMod);
-
-  assert(topMod->hasDef());
-
-  c->runPasses({"rungenerators",
-        //"removeconstduplicates",
-        "flatten",
-        //"removeconstduplicates",
-        "packconnections"});
-
-  cout << "Done with packing connections" << endl;
-
-
-  cout << "# of instances in flattened circuit = " << topMod->getDef()->getInstances().size() << endl;
-
-  ModuleDef* def = topMod->getDef();
-
-  // TODO: Pass in port path names
-  vector<Wireable*> subCircuitPorts{def->sel("self")->sel("config_addr"),
-      def->sel("self")->sel("config_data"),
-      def->sel("self")->sel("clk"),
-      def->sel("self")->sel("reset")};
-      //def->sel("self")->sel("tile_id")};
-  
-  auto subCircuitInstances =
-    extractSubcircuit(topMod, subCircuitPorts);
-
-  cout << "# of instances in subciruit = " << subCircuitInstances.size() << endl;
-
-  // Create the subcircuit for the config
-  addSubcircuitModule("topMod_config",
-                      topMod,
-                      subCircuitPorts,
-                      subCircuitInstances,
-                      c,
-                      c->getGlobal());
-
-  Module* topMod_conf =
-    c->getGlobal()->getModule("topMod_config");
-
-  // cout << "topMod_config interface" << endl;
-  // cout << topMod_conf->toString() << endl;
-
-  assert(topMod_conf != nullptr);
-  assert(topMod_conf->hasDef());
-
-  deleteDeadInstances(topMod_conf);
-
-  cout << "# of instances in subcircuit after deleting dead instances = " << topMod_conf->getDef()->getInstances().size() << endl;
-  
-  c->runPasses({"removeconstduplicates"}); //, "cullgraph"});
-
-  cout << "# of instances in subcircuit after deleting duplicate constants = " << topMod_conf->getDef()->getInstances().size() << endl;
-  
-  // cout << "Saving the config circuit" << endl;
-  // if (!saveToFile(c->getGlobal(), "topModConfig.json", topMod_conf)) {
-  //   cout << "Could not save to json!!" << endl;
-  //   c->die();
-  // }
-
-  cout << "Clockifying the configuration circuit interface" << endl;
-
-  //c->runPasses({"clockifyinterface"});
-
-  cout << "Building simulator state for config" << endl;
-
-  SimulatorState state(topMod_conf);
-  state.setClock("self.clk", 0, 1);
-  state.setValue("self.reset", BitVec(1, 0));
-
-  cout << " Done building simulator state" << endl;
-
-  // TODO: Load simulate config line by line
-
-  // TODO: Partially evaluate the original top module by config
-
-  // TODO: Simulate the partially evaluated circuit
-  deleteContext(c);
-  
-}
-
 BitVector hexStringToBitVector(const std::string& str) {
   vector<char> addrBytes = hexToBytes(str);
 
@@ -220,6 +125,129 @@ BitStreamConfig loadConfig(const std::string& configFileName) {
   assert(configDatas.size() == configLines.size());
 
   return {configAddrs, configDatas};
+}
+
+void processTop(const std::string& fileName,
+                const std::string& topModName) {
+  Context* c = newContext();
+
+  CoreIRLoadLibrary_rtlil(c);
+
+  Module* topMod = nullptr;
+
+  if (!loadFromFile(c, fileName, &topMod)) {
+    cout << "Could not Load from json!!" << endl;
+    c->die();
+  }
+
+  topMod = c->getGlobal()->getModule(topModName);
+  c->setTop(topMod);
+
+  assert(topMod->hasDef());
+
+  c->runPasses({"rungenerators",
+        //"removeconstduplicates",
+        "flatten",
+        //"removeconstduplicates",
+        "packconnections"});
+
+  cout << "Done with packing connections" << endl;
+
+
+  cout << "# of instances in flattened circuit = " << topMod->getDef()->getInstances().size() << endl;
+
+  ModuleDef* def = topMod->getDef();
+
+  // TODO: Pass in port path names
+  vector<Wireable*> subCircuitPorts{def->sel("self")->sel("config_addr"),
+      def->sel("self")->sel("config_data"),
+      def->sel("self")->sel("clk"),
+      def->sel("self")->sel("reset")};
+      //def->sel("self")->sel("tile_id")};
+  
+  auto subCircuitInstances =
+    extractSubcircuit(topMod, subCircuitPorts);
+
+  cout << "# of instances in subciruit = " << subCircuitInstances.size() << endl;
+
+  // Create the subcircuit for the config
+  addSubcircuitModule("topMod_config",
+                      topMod,
+                      subCircuitPorts,
+                      subCircuitInstances,
+                      c,
+                      c->getGlobal());
+
+  Module* topMod_conf =
+    c->getGlobal()->getModule("topMod_config");
+
+  // cout << "topMod_config interface" << endl;
+  // cout << topMod_conf->toString() << endl;
+
+  assert(topMod_conf != nullptr);
+  assert(topMod_conf->hasDef());
+
+  deleteDeadInstances(topMod_conf);
+
+  cout << "# of instances in subcircuit after deleting dead instances = " << topMod_conf->getDef()->getInstances().size() << endl;
+
+  c->setTop(topMod_conf);
+  c->runPasses({"removeconstduplicates", "cullgraph"});
+
+  cout << "# of instances in subcircuit after deleting duplicate constants = " << topMod_conf->getDef()->getInstances().size() << endl;
+  
+  cout << "Saving the config circuit" << endl;
+  if (!saveToFile(c->getGlobal(), "topModConfig.json", topMod_conf)) {
+    cout << "Could not save to json!!" << endl;
+    c->die();
+  }
+
+  cout << "Clockifying the configuration circuit interface" << endl;
+
+  c->runPasses({"clockifyinterface"});
+
+  cout << "Building simulator state for config" << endl;
+
+  SimulatorState state(topMod_conf);
+  state.setClock("self.clk", 0, 1);
+  state.setValue("self.reset", BitVec(1, 0));
+
+  cout << " Done building simulator state" << endl;
+
+  BitStreamConfig bs = loadConfig("./bitstream/shell_bitstream.bs");
+  for (uint i = 0; i < bs.configAddrs.size(); i++) {
+    cout << "Simulating config " << i << endl;
+    state.setValue("self.config_addr", bs.configAddrs[i]);
+    state.setValue("self.config_data", bs.configDatas[i]);
+
+    state.execute();
+    state.execute();
+    
+  }
+
+  cout << "Done simulating config" << endl;
+  // registersToConstants(miniChip, state.getCircStates().back().registers);
+  // deleteDeadInstances(miniChip);
+  // unpackConnections(miniChip);
+  // foldConstants(miniChip);
+  // deleteDeadInstances(miniChip);
+
+  // c->runPasses({"packconnections"});
+
+  // cout << "miniChip partially evaluated instances" << endl;
+  // for (auto instR : miniChip->getDef()->getInstances()) {
+  //   cout << "\t" << instR.second->toString() << endl;
+  // }
+
+  // cout << "miniChip partially evaluated connections" << endl;
+  // for (auto conn : miniChip->getDef()->getConnections()) {
+  //   cout << "\t" << conn.first->toString() << " <-> " << conn.second->toString() << endl;
+  // }
+
+  // assert(miniChip->getDef()->getInstances().size() == 2);
+  
+  deleteContext(c);
+  
 }
 
 int main() {
