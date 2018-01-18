afe938fa8448e81562869b208072f4ff72f41d37
Dillon Huff <dillonhuff@gmail.com>
2018-01-18 13:21:30 -0800

    Need to accelerate constant folding

--- partial_evaluator.cpp
+++ partial_evaluator.cpp
@@ -249,7 +249,8 @@ void processTop(const std::string& fileName,
   
 }
 
-void simulateConfig(const std::string& fileName) {
+void simulateConfig(const std::string& configFileName,
+                    const std::string& topFileName) {
 
   Context* c = newContext();
 
@@ -257,7 +258,7 @@ void simulateConfig(const std::string& fileName) {
 
   Module* topMod_conf = nullptr;
 
-  if (!loadFromFile(c, fileName, &topMod_conf)) {
+  if (!loadFromFile(c, configFileName, &topMod_conf)) {
     cout << "Could not Load from json!!" << endl;
     c->die();
   }
@@ -292,6 +293,64 @@ void simulateConfig(const std::string& fileName) {
 
   cout << "Done simulating config" << endl;
 
+  Module* wholeTopMod = nullptr;
+
+  if (!loadFromFile(c, topFileName, &wholeTopMod)) {
+    cout << "Could not Load from json!!" << endl;
+    c->die();
+  }
+
+  c->runPasses({"rungenerators",
+        //"removeconstduplicates",
+        "flatten",
+        //"removeconstduplicates",
+        "packconnections"});
+
+
+  auto regMap = state.getCircStates().back().registers;
+  cout << "Converting " << regMap.size() << " registers to constants" << endl;
+
+  registersToConstants(wholeTopMod, state.getCircStates().back().registers);
+
+  cout << "Deleting dead instances" << endl;
+  deleteDeadInstances(wholeTopMod);
+
+  cout << "# of instances partially evaluated top after deleting dead instances = " << wholeTopMod->getDef()->getInstances().size() << endl;
+
+  unpackConnections(wholeTopMod);
+
+  cout << "Folding constants" << endl;
+  foldConstants(wholeTopMod);
+
+  cout << "Done folding constants" << endl;
+
+  deleteDeadInstances(wholeTopMod);
+
+  cout << "# of instances partially evaluated top after constant folding = " << wholeTopMod->getDef()->getInstances().size() << endl;
+
+  c->runPasses({"packconnections"});
+
+
+  c->setTop(wholeTopMod);
+
+  c->runPasses({"cullgraph"});
+  if (!saveToFile(c->getGlobal(), "partialEvalTopMod.json", wholeTopMod)) {
+    cout << "Could not save to json!!" << endl;
+    c->die();
+  }
+  
+  // cout << "miniChip partially evaluated instances" << endl;
+  // for (auto instR : miniChip->getDef()->getInstances()) {
+  //   cout << "\t" << instR.second->toString() << endl;
+  // }
+
+  // cout << "miniChip partially evaluated connections" << endl;
+  // for (auto conn : miniChip->getDef()->getConnections()) {
+  //   cout << "\t" << conn.first->toString() << " <-> " << conn.second->toString() << endl;
+  // }
+
+  // assert(miniChip->getDef()->getInstances().size() == 2);
+  
   deleteContext(c);
   
 }
@@ -302,9 +361,9 @@ int main() {
   // NOTE: Must change every time yosys is run!
   string fileName = "top.json"; //"__DOLLAR__paramod__BACKSLASH__test_lut__BACKSLASH__DataWidth__EQUALS__1.json";
 
-  processTop(fileName, modName);
+  //processTop(fileName, modName);
 
-  //simulateConfig("topModConfig.json");
+  simulateConfig("topModConfig.json", "top.json");
   
 
 }
