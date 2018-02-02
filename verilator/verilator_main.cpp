3cb34ea0723b3de97a79c787f87844826f2bdd7d
Dillon Huff <dillonhuff@gmail.com>
2018-02-02 15:15:04 -0800

    Config data looking sane. Now need to convert away dollar signs since verilator does not like them

--- verilator/verilator_main.cpp
+++ verilator/verilator_main.cpp
@@ -2,7 +2,7 @@
 #include <ctime>
 #include <cstdlib>
 #include <vector>
-
+#include <sstream>
 #include <string>
 #include <fstream>
 #include <streambuf>
@@ -25,7 +25,11 @@ int main(int argc, char** argv) {
   // Run config on the circuit
 
   top->clk = 0;
-  top->tile_id = 0;
+  top->tile_id = 1;
+  top->reset = 0;
+
+  top->eval();
+
 
   std::vector<std::string> strings;
 
@@ -46,6 +50,41 @@ int main(int argc, char** argv) {
     cout << strings[i] << endl;
   }
 
+  for (int i = 0; i < strings.size(); i++) {
+
+    cout << "Evaluating " << i << endl;
+
+    string addrStr = strings[i].substr(0, 8);
+
+    unsigned int configAddr;
+    std::stringstream ss;
+    ss << std::hex << addrStr;
+    ss >> configAddr;
+
+    string dataStr = strings[i].substr(9, 18);    
+
+    unsigned int configData;
+    std::stringstream ss2;
+    ss2 << std::hex << dataStr;
+    ss2 >> configData;
+
+    cout << "\taddrStr = " << addrStr << endl;
+    cout << "\tdataStr = " << dataStr << endl;
+
+    top->config_addr = configAddr; // Insert config
+    top->config_data = configData; // Insert data
+    top->clk = 0;
+    top->eval();
+    
+    top->clk = 1;
+    top->eval();
+
+  }
+
+  cout << "Top registers" << endl;
+  cout << std::hex << (int) top->test_pe__024test_opt_reg_d__024___05FDOLLAR___05Fprocdff___05FDOLLAR___05F1148__024reg0_subcircuit_out << endl;
+  cout << std::hex << (int) top->test_pe__024test_opt_reg_b__024___05FDOLLAR___05Fprocdff___05FDOLLAR___05F1147__024reg0_subcircuit_out << endl;
+  
   // Write out the register values
 
 }
