3434619eb315ae8430bc6ab630090437729391ed
Dillon Huff <dillonhuff@gmail.com>
2018-02-02 15:02:10 -0800

    Can now split the config into lines

--- verilator/verilator_main.cpp
+++ verilator/verilator_main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+#include <vector>
+
+#include <string>
+#include <fstream>
+#include <streambuf>
 
 #include "VtopMod_config.h"
 #include "verilated.h"
@@ -12,11 +17,35 @@ int main(int argc, char** argv) {
   VtopMod_config* top = new VtopMod_config;
 
   // Read in config bitstream
+  std::ifstream t("../bitstream/shell_bitstream.bs");
+  std::string configBits((std::istreambuf_iterator<char>(t)),
+                         std::istreambuf_iterator<char>());  
 
+  cout << "ConfigBits = " << endl << configBits << endl;
   // Run config on the circuit
 
-  // Write out the register values
+  top->clk = 0;
+  top->tile_id = 0;
 
-  // 
+  std::vector<std::string> strings;
+
+  std::string::size_type pos = 0;
+  std::string::size_type prev = 0;
+  char delimiter = '\n';
+  string str = configBits;
+  while ((pos = str.find(delimiter, prev)) != std::string::npos) {
+    strings.push_back(str.substr(prev, pos - prev));
+    prev = pos + 1;
+  }
+
+  // To get the last substring (or only, if delimiter is not found)
+  strings.push_back(str.substr(prev));
+
+  cout << "Config lines" << endl;
+  for (int i = 0; i < strings.size(); i++) {
+    cout << strings[i] << endl;
+  }
+
+  // Write out the register values
 
 }
