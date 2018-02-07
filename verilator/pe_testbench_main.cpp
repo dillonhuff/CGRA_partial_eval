cd4fe66b8848bbc3e5838e8acf1119a5bb700f93
Dillon Huff <dillonhuff@gmail.com>
2018-02-07 12:42:46 -0800

    Simple verilog testbench running

--- verilator/pe_testbench_main.cpp
+++ verilator/pe_testbench_main.cpp
@@ -1,3 +1,29 @@
+#include "verilated.h"
+#include "Vpe_tile_new_unq1.h"
+
+#include <iostream>
+
+using namespace std;
+
 int main() {
+  Vpe_tile_new_unq1* tile = new Vpe_tile_new_unq1();
+  tile->clk = 0;
+  tile->tile_id = 1;
+  tile->reset = 0;
+  tile->eval();
+
+
+  tile->clk = 1;
+  tile->config_addr = 0;
+  tile->config_data = 0;
+
+  tile->in_BUS16_S2_T0 = 4;
+
+  tile->eval();
+
+  cout << "tile->out_BUS16_S1_T0 == " << tile->out_BUS16_S1_T0 << endl;
+
+  assert(tile->out_BUS16_S1_T0 == 4*2);
+
   return 0;
 }
