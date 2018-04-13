ddb4a00a19fa28c21fe5216bc103010a7acba221
Dillon Huff <dillonhuff@gmail.com>
2018-04-13 14:37:34 -0700

    Adding real conv_2_1

--- ncsim_partial_eval/cgra_config_tb.v
+++ ncsim_partial_eval/cgra_config_tb.v
@@ -49,7 +49,32 @@ module cgra_config_tb();
 	    config_addr <= 0;
 	 end
       end
-   end
+   end // always @ (negedge clk_in)
+
+   wire [15:0] pad_S0_T0_in;
+   wire [15:0] pad_S0_T1_in;
+   wire [15:0] pad_S0_T2_in;
+   wire [15:0] pad_S0_T3_in;
+   wire [15:0] pad_S0_T4_in;
+
+   wire [15:0] pad_S1_T0_in;
+   wire [15:0] pad_S1_T1_in;
+   wire [15:0] pad_S1_T2_in;
+   wire [15:0] pad_S1_T3_in;
+   wire [15:0] pad_S1_T4_in;
+
+   wire [15:0] pad_S2_T0_in;
+   wire [15:0] pad_S2_T1_in;
+   wire [15:0] pad_S2_T2_in;
+   wire [15:0] pad_S2_T3_in;
+   wire [15:0] pad_S2_T4_in;
+
+   wire [15:0] pad_S3_T0_in;
+   wire [15:0] pad_S3_T1_in;
+   wire [15:0] pad_S3_T2_in;
+   wire [15:0] pad_S3_T3_in;
+   wire [15:0] pad_S3_T4_in;
+   
    
    top cgra(.config_addr_in(config_addr_in),
             .config_data_in(config_data_in),
